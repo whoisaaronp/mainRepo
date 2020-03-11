ced0c6f1a152a1ba2030c277814b25eb17f93978
Paterson <a_paterson@ats-hr328-dm09.local>
2020-03-11 15:34:40 -0400

    working scrapy flask file

---
+++ web_flask/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+import json
+import requests
+from datetime import datetime
+import pytz
+
+
+app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://admin:123@mongo:27017/movie_cms'
+mongo = PyMongo(app)
+
+
+# set up your route
+@app.route('/')
+def index():
+	return '<h1>This is a Flask CMS</h1>'
+
+# building the second route
+@app.route('/fetch')
+def fetch():
+	params = {
+		'spider_name': 'movie',
+		'start_requests': True
+	}
+	response = requests.get('http://scrapy:9080/crawl.json', params)
+	return response.text
+
