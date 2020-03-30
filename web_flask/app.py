c85d490488a2597e51b7549091ee0f52a2fcd3e0
Aaron Paterson <juniha.aaron@gmail.com>
2020-03-30 15:09:20 -0400

    updated and added more pages

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from flask import Flask, render template
+# from flask_pymongo import PyMongo
 import json
 import requests
 from datetime import datetime
@@ -7,22 +7,27 @@ import pytz
 
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://admin:123@mongo:27017/movie_cms'
-mongo = PyMongo(app)
-
+# app.config['MONGO_URI'] = 'mongodb://admin:123@mongo:27017/movie_cms'
+# mongo = PyMongo(app)
+mongodb+srv://juniha:Rodyroem@cluster0-naqor.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
 
 # set up your route
 @app.route('/')
 def index():
-	return '<h1>This is a Flask CMS</h1>'
+	return render_template('home.html')
 
-# building the second route
-@app.route('/fetch')
-def fetch():
-	params = {
-		'spider_name': 'movie',
-		'start_requests': True
-	}
-	response = requests.get('http://scrapy:9080/crawl.json', params)
-	return response.text
+# set up your barchart
+@app.route('/bar_chart')
+def bar_chart():
+	return render_template('barchart.html')
+
+# # building the second route
+# @app.route('/fetch')
+# def fetch():
+# 	params = {
+# 		'spider_name': 'movie',
+# 		'start_requests': True
+# 	}
+# 	response = requests.get('http://scrapy:9080/crawl.json', params)
+# 	return response.text
 
