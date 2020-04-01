dd9e68541f7c9bfa21d1998415256c280b7bff40
Aaron Paterson <juniha.aaron@gmail.com>
2020-04-01 17:51:50 -0400

    updating repo with app.py

--- web_flask/app.py
+++ web_flask/app.py
@@ -7,9 +7,8 @@ import pytz
 
 
 app = Flask(__name__)
-# app.config['MONGO_URI'] = 'mongodb://admin:123@mongo:27017/movie_cms'
-# mongo = PyMongo(app)
-mongodb+srv://juniha:Rodyroem@cluster0-naqor.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
+
+# mongodb+srv://juniha:Rodyroem@cluster0-naqor.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
 
 # set up your route
 @app.route('/')
@@ -36,13 +35,4 @@ def pie_chart():
 def bubble_chart():
 	return render_template('bubblechart.html')
 
-# # building the second route
-# @app.route('/fetch')
-# def fetch():
-# 	params = {
-# 		'spider_name': 'movie',
-# 		'start_requests': True
-# 	}
-# 	response = requests.get('http://scrapy:9080/crawl.json', params)
-# 	return response.text
 
